98ada763ed5b732c149bf25732f790d61df8f694
Louis Gautier <Louis.gautier@outlook.fr>
2023-08-23 17:18:25 +0200

    Dealing with database / Docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
 val postgresql_version: String by project
+val hikaricp_version: String by project
+val ehcache_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.0"
@@ -55,7 +57,9 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.postgresql:postgresql:$postgresql_version")
-    implementation("org.postgresql:postgresql:$postgresql_version")
+
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    implementation("org.ehcache:ehcache:$ehcache_version")
 
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-auth-jvm")
--- src/main/kotlin/com/api/base/database/DatabaseFactory.kt
+++ src/main/kotlin/com/api/base/database/DatabaseFactory.kt
@@ -1,5 +1,7 @@
 package com.api.base.database
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.config.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.Serializable
@@ -11,20 +13,38 @@ import java.sql.Connection
 
 object DatabaseFactory {
     fun init(config: ApplicationConfig) {
-        val driverClassName = config.property("ktor.storage.driverClassName").getString()
-        val jdbcURL = config.property("ktor.storage.jdbcURL").getString()
-        TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
-        val database = Database.connect(
-            jdbcURL,
-            driverClassName,
-            databaseConfig = DatabaseConfig {
-                sqlLogger = Slf4jSqlDebugLogger
-            }
+        val driverClassName = config.property("ktor.database.driverClassName").getString()
+        val jdbcURL = config.property("ktor.database.jdbcURL").getString()
+        val maxPoolSize = config.property("ktor.database.maxPoolSize").getString()
+        val autoCommit = config.property("ktor.database.autoCommit").getString()
+        val username = config.property("ktor.database.user").getString()
+        val password = config.property("ktor.database.password").getString()
+        val defaultDatabase = config.property("ktor.database.database").getString()
+        val connectionPool = createHikariDataSource(
+            url = "$jdbcURL/$defaultDatabase?user=$username&password=$password",
+            driver = driverClassName,
+            maxPoolSize.toInt(),
+            autoCommit.toBoolean()
         )
+        val database = Database.connect(connectionPool)
         transaction(database) {
             SchemaUtils.create(Articles)
         }
     }
+
+    private fun createHikariDataSource(
+        url: String,
+        driver: String,
+        maxPoolSize: Int,
+        autoCommit: Boolean
+    ) = HikariDataSource(HikariConfig().apply {
+        driverClassName = driver
+        jdbcUrl = url
+        maximumPoolSize = maxPoolSize
+        isAutoCommit = autoCommit
+        transactionIsolation = "TRANSACTION_SERIALIZABLE"
+        validate()
+    })
 
     suspend fun <T> dbQuery(block: suspend () -> T): T =
         newSuspendedTransaction(Dispatchers.IO) { block() }
