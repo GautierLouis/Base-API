17a2e44a18ec953bd7f0c69665c5beb4aa21465b
Louis Gautier <Louis.gautier@outlook.fr>
2023-08-26 11:36:29 +0200

    Put back database

--- src/main/kotlin/com/api/base/database/DatabaseFactory.kt
+++ src/main/kotlin/com/api/base/database/DatabaseFactory.kt
@@ -5,11 +5,11 @@ import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.config.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.Serializable
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.sql.Connection
 
 object DatabaseFactory {
     fun init(config: ApplicationConfig) {
@@ -20,16 +20,16 @@ object DatabaseFactory {
         val username = config.property("ktor.database.user").getString()
         val password = config.property("ktor.database.password").getString()
         val defaultDatabase = config.property("ktor.database.database").getString()
-//        val connectionPool = createHikariDataSource(
-//            url = "$jdbcURL/$defaultDatabase?user=$username&password=$password",
-//            driver = driverClassName,
-//            maxPoolSize.toInt(),
-//            autoCommit.toBoolean()
-//        )
-//        val database = Database.connect(connectionPool)
-//        transaction(database) {
-//            SchemaUtils.create(Articles)
-//        }
+        val connectionPool = createHikariDataSource(
+            url = "$jdbcURL/$defaultDatabase?user=$username&password=$password",
+            driver = driverClassName,
+            maxPoolSize.toInt(),
+            autoCommit.toBoolean()
+        )
+        val database = Database.connect(connectionPool)
+        transaction(database) {
+            SchemaUtils.create(Articles)
+        }
     }
 
     private fun createHikariDataSource(
